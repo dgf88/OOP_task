f43c2306c257e97cf0d384dfee62fe3058926c8b
Victoria Rybalko <rybalko.victoria@gmail.com>
2020-07-10 17:03:34 +0300

    Add getters and setters for properties days, rate

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 class Worker {
-    constructor(name, surname, rate, days = 0) {
+    constructor(name, surname, rate = 0, days = 0) {
         this.name = name;
         this.surname = surname;
-        this.rate = rate;
-        this.days = days;
+        this._rate = rate;
+        this._days = days;
     }
 
     getSalary() {
         return this.rate * this.days;
     }
+
+    get rate() {
+        return this._rate;
+    }
+
+    set rate(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('Property Rate must be number type');
+        }
+        if (value < 0 || !Number.isInteger(value)) {
+            throw new RangeError('Property Rate must be positive integer');
+        }
+        this._rate = value;
+    }
+
+    get days() {
+        return this._days;
+    }
+
+    set days(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('Property Days must be number type');
+        }
+        if (value < 0 || !Number.isInteger(value)) {
+            throw new RangeError('Property Days must be positive integer');
+        }
+        this._days = value;
+    }
+
 }
 
 const worker1 = new Worker("John", "Miller", 100, 20);
