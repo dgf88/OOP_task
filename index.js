dc79428d6d1e7b39e38149c47c9d1059cc896d46
Victoria Rybalko <rybalko.victoria@gmail.com>
2020-07-10 15:47:23 +0300

    Add class Worker

---
+++ index.js
@@ -0,0 +1,12 @@
+class Worker {
+    constructor(name, surname, rate, days) {
+        this.name = name;
+        this.surname = surname;
+        this.rate = rate;
+        this.days = days;
+    }
+
+    getSalary() {
+        return rate * days;
+    }
+}
